3cc1e5bd1c113457efad6298fe2c4921cd9378b1
Payam <payambapiri.97@gmail.com>
2021-08-20 02:15:42 +0430

    Clean-up

--- make.py
+++ make.py
@@ -1,22 +1,28 @@
 from os import listdir
 
 # Functions: HTML Template
-def get_head (dir, lang, style):
+def get_head(dir, lang, style):
         return f'''
         <html dir="{dir}" lang="{lang}">
         <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width">
         <style>{style}</style>
         </head><body>
         <h1>سەرچاوەکانی دەیتا(Data)ی زمانی کوردی</h1>
         '''
 
-def get_style ():
+def get_style():
         return '''
+        @font-face {
+        font-family:'nesx';
+        font-display:swap;
+        src:url("//allekok.github.io/src/frontend/style/DroidNaskh-Regular.woff2") format("woff2")
+        }
         body {
         max-width:1000px;
         margin:0 auto;
+        font-family:'nesx';
         font-size:17px;
         line-height:2;
         padding:2em 0
@@ -35,8 +41,11 @@ def get_style ():
         opacity:.6
         }
         .entry {
-        padding:.3em .5em;
+        padding:1em;
         color:#333
+        }
+        .entry:nth-child(odd) {
+        background:#EFEFEF;
         }
         .entry p {
         margin:0
@@ -63,26 +72,32 @@ def get_style ():
         }
         '''
 
-def get_script (): return ''
+def get_script():
+        return ''
 
-def get_foot (script):
+def get_foot(script):
         return f'<script>{script}</script></body></html>'
 
 # Functions: Parser
-def lines (text):
+def lines(text):
         return text.strip().split('\n')
-def pair (line):
+
+def pair(line):
         return [x.strip() for x in line.split(':', 1)]
-def parse (text):
+
+def parse(text):
         return [pair(x) for x in lines(text)]
+
 def k(pair): return pair[0]
+
 def v(pair): return pair[1]
-def assoc (lst, key):
+
+def assoc(lst, key):
         for x in lst:
                 if key == k(x): return x
 
 # Functions: Misc.
-def kurdish_numbers (str):
+def kurdish_numbers(str):
         eng = ['1','2','3','4','5','6','7','8','9','0']
         ckb = ['١','٢','٣','٤','٥','٦','٧','٨','٩','٠']
         for i in range(len(eng)):
@@ -91,35 +106,37 @@ def kurdish_numbers (str):
 
 # Constants
 LIST_DIR = 'LIST'
-SITE_DIR = 'site'
 FOOT = get_foot(get_script())
 STYLE = get_style()
+LANG = 'ckb'
+DIR = 'rtl'
 
 # Run
-for lang in listdir(LIST_DIR):
-        # head
-        if lang.endswith('ar'): dir = 'rtl'
-        else: dir = 'ltr'
-        lng = lang[0:lang.find('.')]
-        HEAD = get_head(dir,lng,STYLE)
-        
-        # body
-        body = ''; i = 0
-        lang_path = f'{LIST_DIR}/{lang}'
-        filelist = listdir(lang_path)
-        filelist.sort(reverse=True, key=lambda x:int(x))
-        for fname in filelist:
-                fpath = f'{lang_path}/{fname}'
-                with open(fpath, 'r') as f:
-                        text = f.read()
-                        lst = parse(text)
-                        name = assoc(lst, 'ناو')
-                        href = assoc(lst, 'نیشانی')
-                        link = ''
+
+# head
+HEAD = get_head(DIR, LANG, STYLE)
+
+# body
+body = ''
+i = 0
+filelist = listdir(LIST_DIR)
+filelist.sort(reverse=True, key=lambda x:int(x))
+for fname in filelist:
+        fpath = f'{LIST_DIR}/{fname}'
+        with open(fpath, 'r') as f:
+                text = f.read()
+                lst = parse(text)
+                name = assoc(lst, 'ناو')
+                href = assoc(lst, 'نیشانی')
+                link = ''
 
                 if name and href:
-                        link = f'<a href="../{fpath}"><small>[TXT]</small></a>{kurdish_numbers(fname)}. <a href="{v(href)}">{v(name)}</a>'
-                
+                        link = f'''
+                        <a href="../{fpath}"><small>[TXT]</small></a>
+                        {kurdish_numbers(fname)}. <a href="{v(href)}"
+                        >{v(name)}</a>
+                        '''
+                        
                 body += f'''
                 <div class="entry">
                 <p>{link}</p>
@@ -128,15 +145,17 @@ for lang in listdir(LIST_DIR):
                 
                 for p in lst:
                         key = k(p)
-                        if key == 'ناو': continue
+                        if key == 'ناو':
+                                continue
                         body += f'<li>{key}: {v(p)}</li>'
 
                 body += '</ul></div>'
                 
                 # foot
-                html = (HEAD+body+FOOT).replace('\n','').replace('        ','')
-                with open(f'{SITE_DIR}/list.{lang}.html', 'w') as f:
+                html = HEAD + body + FOOT
+                html = html.replace('\n','').replace('        ','')
+                with open('index.html', 'w') as f:
                         f.write(html)
-        
-        # done
-        print(lang, 'done')
+                        
+# done
+print('done')
